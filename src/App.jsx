0ed5c5f3aee066cbc3a908fc392b673796aca4a7
Tracy Karanja <chirutracy@gmail.com>
2024-05-06 19:05:46 +0300

    Added my project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Outlet } from "react-router-dom";
+import NavBar from "./components/NavBar";
+
+function App() {
+
+  return (
+    <>
+      <header>
+        <NavBar />
+      </header>
+      <Outlet />
+    </>
+  );
+}
+
+export default App;
---
+++ src/routes.js
@@ -0,0 +1,39 @@
+import BusinessFinance from "./pages/BusinessFinance"
+import DifferentNews from "./pages/DifferentNews";
+import Entertainment from "./pages/Entertainment";
+import Politics from "./pages/Politics";
+import Sports from "./pages/Sports";
+import Home from "./pages/Home"
+import ErrorPage from "./pages/ErrorPage"
+
+const routes = [
+  {
+    path: "/",
+    element: <Home />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/othernews",
+        element: <DifferentNews />,
+      },
+      {
+        path: "/finance",
+        element: <BusinessFinance />,
+      },
+      {
+        path: "/politics",
+        element: <Politics />,
+      },
+      {
+        path: "/sports",
+        element: <Sports />,
+      },
+      {
+        path: "/entertainment",
+        element: <Entertainment />,
+      },
+    ],
+  },
+];
+
+export default routes;
