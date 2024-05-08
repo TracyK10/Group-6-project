9f49a061558a76187c9cf209925cdcaf531031f0
Desma-2000 <60510121+Desma-2000@users.noreply.github.com>
2024-05-08 16:57:33 +0300

    Update BusinessFinance.jsx

--- src/pages/BusinessFinance.jsx
+++ src/pages/BusinessFinance.jsx
@@ -1,11 +1,33 @@
 
 
+import React from "react";
+
 function BusinessFinance() {
   return (
     <div>
+      <h2>Business & Finance</h2>
+      <p>Welcome to the Business & Finance section of our website. Here, you'll find valuable information, news, and insights related to business and finance.</p>
       
+      <h3>Latest Business News</h3>
+      <ul>
+        <li>News Item 1</li>
+        <li>News Item 2</li>
+        <li>News Item 3</li>
+        {/*placeholders with actual news items */}
+      </ul>
+
+      <h3>Financial Tips</h3>
+      <p>Here are some tips to help you manage your finances effectively:</p>
+      <ul>
+        <li>Tip 1: Budgeting</li>
+        <li>Tip 2: Investing wisely</li>
+        <li>Tip 3: Saving for retirement</li>
+        { /*placeholders with actual financial tips */}
+      </ul>
+
+      {/* Add more sections or content as needed */}
     </div>
-  )
+  );
 }
 
-export default BusinessFinance
+export default BusinessFinance;
